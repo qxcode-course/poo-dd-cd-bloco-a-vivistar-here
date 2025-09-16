90a0dcc1d28cdd9e519b73bce6fdf848b1444199
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-09-16 20:22:35 +0000

    criando toalha :D

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,20 @@
-print('minha toalha é branca')
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color = 'white'
+        self.size = 'm'
+        self.wetness = 0
+    
+    def __str__(self):
+        return f'color:{self.color}, tam: {self.size}, um: {self.wetness}'
+
+
+towel = Towel('green', 'g') #objetos
+toalha = Towel('red', 'p')
+outra = toalha
+outra.color = 'blue'
+toalha.color = 'yellow'
+
+
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
