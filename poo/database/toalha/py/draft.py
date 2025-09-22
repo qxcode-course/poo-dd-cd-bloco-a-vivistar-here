0cc61f0bb9ea1dbe2486c6b939eebb10d570c329
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-09-22 12:08:16 +0000

    programando a toalha pra funcionar/ser usada (corrigir em sala)

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,20 +1,16 @@
 class Towel:
-    def __init__(self, color: str, size: str):
+    def __init__(self, color: str, size: str): #método construtor, constrói a classe do objeto
         self.color = 'white'
         self.size = 'm'
         self.wetness = 0
     
     def __str__(self):
         return f'color:{self.color}, tam: {self.size}, um: {self.wetness}'
+    def __use__(self, quantidade) #recebe o valor
+        self.wetness = quantidade #wtness aumenta pelo valor passado (ex:15, 20, etc)
+        print('Towel was used. Wetness at {self.wetness}' )
 
 
 towel = Towel('green', 'g') #objetos
-toalha = Towel('red', 'p')
-outra = toalha
-outra.color = 'blue'
-toalha.color = 'yellow'
 
-
-print(towel.color)
-print(towel.size)
-print(towel.wetness)
+#por que esse código tava tão longe...
