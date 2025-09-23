affc1b3db3332375490838bcb9c79f8c6a98c26e
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-09-23 20:10:01 +0000

    reescrever amanhã

---
+++ poo/database/animal/py/draft.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, species: str, sound: str):
+        self.species = species
+        self.sound = sound
+        self.age = 0
+
+    def __str__(self) -> str:
+        return f'species:{self.species}, sound:{self.sound}, age:{self.age}'
+
+def main():
+    animal: Animal = Animal (' ', ' ')
+    while True:
+        line = input()
+        print("$" + line)
+        args = line.split(' ')
+
+        if args[0] == 'end':
+            break
+        elif args[0] == 'show':
+            print(Animal)
+        else:
+            print('fail: comando invalido')
+
+        
