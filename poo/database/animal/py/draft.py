7034e6af76ba1cd2fa2605ab5dfbd68cb2f7cb5c
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-02 18:49:06 +0000

    fazendo  som, main e apanhando

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -4,10 +4,17 @@ class Animal:
         self.sound = sound
         self.age = 0
 
-    def ageBye(self, increment: int) -> None:
+    def ageBy(self, increment: int) -> None:
         self.age += increment
         if self.age >= 4:
             print(f"Warning:{self.species} morreu")
+
+    def makeSound(self = str):
+        if self.age == 0:
+            return "---"
+        elif self.age == 4:
+            return "RIP"
+        return self.sound
 
     def __str__(self) -> str:
         return f'species:{self.species}, sound:{self.sound}, age:{self.age}'
@@ -16,18 +23,19 @@ class Animal:
 
 
 def main():
-    
+    animal: Animal = Animal(" "," ")
     while True:
         line = input()
         print("$" + line)
-        args = line.split(' ')
+        args: list[str] = line.split(" ")
 
-        if args[0] == 'end':
+        if args[0] == "end":
             break
-        elif args[0] == 'show':
-            print(Animal)
-        else:
-            print('fail: comando invalido')
+        elif args[0] == "init":
+            species: str = args[1]
+            sound: str = args[2]
+            animal = Animal(species, sound)
+        elif args
 
 main()
 
