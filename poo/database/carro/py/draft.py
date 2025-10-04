43b1d1d10032520e787ddd73e0064ef950041840
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-04 20:42:43 +0000

    construtor do carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,37 +1,12 @@
-class Car:
-    
+class Carro:
+    def __init__(self):
+        self.pass_ = 0
+        self.km = 0
+        self.passMax = 2
+        self.gas = 0
+        self.gasMax = 100
+        
+    def __str__(self):
+        return f"pass:{self.pass_}, gas:{self.gas}, km:{self.km}"
 
-
-def main():
-    while True:
-        line = input()
-        print("$" + line)
-
-        par = line.split(" ")
-        cmd = par[0]
-
-        if cmd == "end":
-            break
-        elif cmd == "show":
-            # MOSTRE O CARRO
-            pass
-        elif cmd == "enter":
-            # TENTE EMBARCAR UMA PESSOA
-            pass
-        elif cmd == "leave":
-            # TENTE DESEMBARCAR UMA PESSOA
-            pass
-        elif cmd == "fuel":
-            # INCREMENTE O COMBUSTIVEL
-            # increment = int(par[1])
-            pass
-        elif cmd == "drive":
-            # TENTE DIRIGIR CERTA DISTANCIA
-            # distance = int(par[1])
-            pass
-        else:
-            print("fail: comando invalido")
-
-if __name__ == "__main__":
-    main()
-
+        
