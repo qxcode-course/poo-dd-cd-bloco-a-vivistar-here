80407e68399051feb524ef7e613d2846613168c9
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-05 19:32:26 +0000

    construtores

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -3,5 +3,30 @@ class Calculadora:
         self.batteryMax = batteryMax
         self.battery = 0
         self.display = 0.0
+    
+    def change(self, value: int):
+        self.battery += value
+        if self.battery > self.batteryMax:
+            self.battery = self.batteryMax
 
+    def sum(self, a: int, b: int):
+        if self.battery == 0:
+            print("bateria insuficiente")
+            return
+        self.battery -= 1
+        self.display =  a + b
+
+    def div(self, num: int, den: int):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        if den == 0:
+            print("fail: divisao por zero")
+            self.battery -= 1
+            return
+        self.battery -= 1
+        self.display = num / den
+
+    def __str__(self):
+        return f"display = {self.display}, battery = {self.battery}"
     
